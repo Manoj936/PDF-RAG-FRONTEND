803fab78b8c97ba23be6aa345a60db6a7a03ab95
Manoj <manoj.mohapatra986@gmail.com>
2025-06-12 15:21:18 +0530

    adding force dynamic

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,4 +1,6 @@
 "use client"
+
+export const dynamic = "force-dynamic";
 import { useEffect } from "react";
 import useAuthStore from "../../store/useAuthStore";
 import { useRouter } from "next/navigation";
